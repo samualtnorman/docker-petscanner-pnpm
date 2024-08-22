483c2e53b95ca6b8076d8cdeb7051850a8d0c033
Samual Norman <me@samual.uk>
2024-08-22 13:27:00 +0100

    add example node http server

---
+++ example/readme.md
@@ -0,0 +1,5 @@
+# Example Node.js HTTP Server
+
+Run `docker compose up --build` then visit http://localhost:13229/.
+
+The pnpm and Node.js versions and port are configurable in the [`compose.yml`](./compose.yml).
---
+++ example/server.js
@@ -0,0 +1,9 @@
+import { Server } from "http"
+
+new Server((request, response) => {
+	console.log(request.method, request.url)
+	response.writeHead(200, { "Content-Type": "text/plain" })
+	response.end("Hello, world!")
+}).listen(80)
+
+process.on(`SIGTERM`, () => process.exit())
